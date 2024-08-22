892c280b4c46ae09d85c011cc30e484a136ff057
Camilo Medina <camedi445@gmail.com>
2024-08-22 17:57:58 -0500

    Update product_detail.dart

--- flutter/ecommerce/presentation/product_detail.dart
+++ flutter/ecommerce/presentation/product_detail.dart
@@ -1,7 +1,36 @@
 import 'package:flutter/material.dart';
 
-class ProductDetail extends StatelessWidget {
+class ProductDetail extends StatefulWidget {
   const ProductDetail({super.key});
+
+  @override
+  State<ProductDetail> createState() => _ProductDetailState();
+}
+
+class _ProductDetailState extends State<ProductDetail> {
+  int _quantity = 0;
+
+  void _removeItemFromCart() {
+    setState(() {
+      _quantity = 0;
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Producto eliminado del carrito'),
+      ),
+    );
+  }
+
+  void _addItemToCart() {
+    setState(() {
+      _quantity++;
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Se ha agregado el producto al carrito'),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,6 +38,14 @@ class ProductDetail extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Detalle de producto'),
           centerTitle: true,
+          actions: [
+            IconButton(
+                onPressed: _removeItemFromCart,
+                icon: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Icon(Icons.delete),
+                ))
+          ],
         ),
         body: Center(
           child: Column(
@@ -26,12 +63,15 @@ class ProductDetail extends StatelessWidget {
                 height: 32.0,
               ),
               const Text(
-                'Café 500gr',
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                'Café 1lb',
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.w500),
               ),
-              const SizedBox(
-                height: 16.0,
+              Text(
+                '$_quantity',
+                style:
+                    const TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
               ),
+              Text('Unidad${_quantity == 1 ? '' : 'es'} de café agregadas'),
               const Padding(
                 padding: EdgeInsets.all(16.0),
                 child: Text(
@@ -53,9 +93,12 @@ class ProductDetail extends StatelessWidget {
             ],
           ),
         ),
-        floatingActionButton: const FloatingActionButton(
-          onPressed: null,
-          child: Icon(Icons.add_shopping_cart),
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 108, 16),
+          child: FloatingActionButton(
+            onPressed: _addItemToCart,
+            child: const Icon(Icons.add_shopping_cart),
+          ),
         ));
   }
 }
